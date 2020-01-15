b84986ebf37e76a48c6e36146230d13ce826680d
MarsLoucetios <35880688+MarsLoucetios@users.noreply.github.com>
2020-01-15 16:14:00 -0700

    Add files via upload

---
+++ dual_display.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+nvidia-settings -a CurrentMetaMode="DPY-1: 1920x1080_60 +0+0, DPY-4: 1600x900_60 +1920+0"
---
+++ single_display.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+nvidia-settings -a CurrentMetaMode="DPY-1: 1920x1080_60 +0+0"
